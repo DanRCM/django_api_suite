82541ce0393b634b1d46a04ea7d0b625ca4c2504
DanRCM <112514991+DanRCM@users.noreply.github.com>
2026-01-07 03:15:30 +0000

    Se añadieron todas las solicitudes

--- demo_rest_api/urls.py
+++ demo_rest_api/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+   # Ruta para GET (lista) y POST (crear)
    path("index/", views.DemoRestApi.as_view(), name="demo_rest_api_resources"),
+   
+   # Ruta para PUT, PATCH, DELETE (necesita el ID)
+   path("<str:id>/", views.DemoRestApiItem.as_view(), name="demo_rest_api_item_resources"),
 ]
